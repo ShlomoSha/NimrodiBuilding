7d1e99f297da322c74249db7ac51f2e19ba31090
shlomoShalem <s0552288304@gmail.com>
2024-11-07 10:32:07 +0200

    role reducer done

--- src/store/rolereducer.ts
+++ src/store/rolereducer.ts
@@ -1,1 +1,22 @@
-//FILL HERE 3.2
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+const initialState = "Unknown Personnel"
+
+export const roleSlice = createSlice({
+    name: 'Role',
+    initialState,
+    reducers: {
+        setRole : (state, action: PayloadAction<string>) => {
+            if (action.payload == "Guest" || action.payload == "Student" || action.payload == "Developer" || action.payload == "Executive Personnel") {
+                state = action.payload
+                return
+            } else {
+                return
+            }
+        }
+    }
+})
+
+export default roleSlice
+
+export const { setRole } = roleSlice.actions
